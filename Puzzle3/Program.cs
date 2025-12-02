3764d6c51d5d69957dc5c5f89c3365c618fd762c
João Filipe Matos Silva <joao@kspace.pt>
2025-12-02 09:47:14 +0000

    Day 2

---
+++ Puzzle3/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+string input = File.ReadAllText("input.txt");
+
+string[] ranges = input.Split(',');
+long sum = 0;
+foreach (var rangeString in ranges)     
+{
+    Console.WriteLine(rangeString);
+    var match = GetRanges().Match(rangeString);
+    if (!match.Success)
+    {
+        continue;
+    }
+    
+    var startString =  match.Groups[1].Value;
+    var endString = match.Groups[2].Value;
+    
+    var start = long.Parse(startString);
+    var end = long.Parse(endString);
+
+    var startLength = startString.Length / 2;
+    var endLength = endString.Length / 2;
+
+    if (startString.Length % 2 == 1)
+    {
+        startLength++;
+    }
+    if (endString.Length % 2 == 1)
+    {
+        endLength++;
+    }
+    
+    for (int div = startLength; div <= endLength; div += 2)
+    {
+        var divider = (int)Math.Pow(10, div);
+        for (long a = start; a <= end; a++)
+        {
+            if (a % divider == a / divider && a / divider * 10 >= divider)
+            {
+                Console.WriteLine($" ==> {a}");
+                sum += a;
+            }
+        }
+    }
+    
+    //Going for the Math way, did not prove so good on Part 2
+    
+}
+
+Console.WriteLine(sum);
+
+
+partial class Program
+{
+    [GeneratedRegex("([0-9]+)-([0-9]+)")]
+    private static partial Regex GetRanges();
+}
---
+++ Puzzle4/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+string input = File.ReadAllText("input.txt");
+
+string[] ranges = input.Split(',');
+long sum = 0;
+foreach (var rangeString in ranges)     
+{
+    Console.WriteLine(rangeString);
+    var match = GetRanges().Match(rangeString);
+    if (!match.Success)
+    {
+        continue;
+    }
+    
+    var startString =  match.Groups[1].Value;
+    var endString = match.Groups[2].Value;
+    
+    var start = long.Parse(startString);
+    var end = long.Parse(endString);
+
+    for (long a = start; a <= end; a++)
+    {
+        var number = a.ToString();
+        if(IsMatch(number))
+        {
+            Console.WriteLine($" => {number}");
+            sum += a;
+        }
+    }
+}
+
+static bool IsMatch(ReadOnlySpan<char> number)
+{
+    // for each possible lenght of sequences (1 to half the lenght of number)
+    for (int i = 1; i <= number.Length / 2; i++)
+    {
+        //if number can't be divided in equal parts, continue
+        if (number.Length % i != 0)
+        {
+            continue;
+        }
+        
+        var needle = number.Slice(0, i);
+        //match until the end
+        int offset = i;
+        do
+        {
+            var test =  number.Slice(offset, i);
+            if (!test.SequenceEqual(needle))
+            {
+                break;
+            }
+
+            offset += i;
+            
+            //if we matched all number, we got a positive
+            if (offset >= number.Length)
+            {
+                return true;
+            }
+        } while(offset < number.Length);
+    }
+    return false;
+}
+
+Console.WriteLine(sum);
+
+
+partial class Program
+{
+    [GeneratedRegex("([0-9]+)-([0-9]+)")]
+    private static partial Regex GetRanges();
+}
