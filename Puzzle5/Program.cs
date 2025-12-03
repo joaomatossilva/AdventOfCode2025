b953283938dfc6b85d1f38cfe45e3e5b287171d4
João Filipe Matos Silva <joao@kspace.pt>
2025-12-03 09:16:17 +0000

    Day 3

---
+++ Puzzle5/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllText("input.txt").Split(Environment.NewLine);
+
+long sum = 0;
+
+foreach (var line in lines)
+{
+    var finalRating = FindMaxCombination(line);
+
+    Console.WriteLine($"{line} => {finalRating}");
+    
+    sum += finalRating;
+}
+
+Console.WriteLine(sum);
+
+
+static int FindMaxCombination(ReadOnlySpan<char> line)
+{
+    int max = 0;
+    //until Length -1 because we need a 2 combination always
+    for (int i = 0; i < line.Length - 1; i++)
+    {
+        var tenths =  (line[i] - '0') * 10;
+
+        //skip if tenths value is already lower
+        if (tenths < max / 10)
+        {
+            continue;
+        }
+        
+        var found = DeepSearch(tenths, line.Slice(i + 1));
+        if (found > max)
+        {
+            max = found;
+        }
+    }
+    
+    return max;
+}
+
+static int DeepSearch(int tenths, ReadOnlySpan<char> line)
+{
+    var max = tenths;
+    foreach (var units in line)
+    {
+        var unit = units - '0';
+        if(tenths + unit > max)
+        {
+            max = tenths + unit;
+        }
+    }
+
+    return max;
+}
---
+++ Puzzle6/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllText("input.txt").Split(Environment.NewLine);
+
+long sum = 0;
+
+foreach (var line in lines)
+{
+    var finalRating = FindMaxForPos(line, 12);
+    
+    Console.WriteLine($"{line} => {finalRating}");
+    
+    sum += finalRating;
+}
+
+Console.WriteLine(sum);
+
+
+static long FindMaxForPos(ReadOnlySpan<char> line, int pos)
+{
+    long max = 0;
+    int maxAt = 0;
+    //Length - pos + 1 => 14 - 2 + => 13 (14 -1)
+    for (int i = 0; i < line.Length - pos + 1; i++)
+    {
+        var val =  line[i] - '0';
+
+        //skip if value is already lower
+        if (val <= max)
+        {
+            continue;
+        }
+
+        max = val;
+        maxAt = i;
+    }
+
+    max *= (long)Math.Pow(10, pos - 1);
+
+    if (pos > 1)
+    {
+        max += FindMaxForPos(line.Slice(maxAt + 1), pos - 1);
+    }
+    
+    return max;
+}
