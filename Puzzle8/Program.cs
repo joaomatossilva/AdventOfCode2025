b926122e5700627df4208895bf825335a1d65fbc
João Filipe Matos Silva <joao@kspace.pt>
2025-12-04 08:07:11 +0000

    Day 4

---
+++ Puzzle8/Program.cs
@@ -0,0 +1,99 @@
+﻿// See https://aka.ms/new-console-template for more information
+var lines = File.ReadAllLines("input.txt");
+
+var grid = new List<bool[]>();
+foreach (var line in lines)
+{
+    var row = new bool[line.Length];
+    for (int i = 0; i < line.Length; i++)
+    {
+        row[i] = line[i] == '@';
+    }
+    grid.Add(row);
+}
+
+
+int count = 0;
+
+int stepCount = 0;
+do
+{
+    stepCount = 0;
+    var outGrid = new List<bool[]>();
+    
+    for (var y = 0; y < grid.Count; y++)
+    {
+        outGrid.Add(new bool[grid[y].Length]);
+        var row = grid[y];
+        for (var x = 0; x < row.Length; x++)
+        {
+            var pos = row[x];
+            if (!pos)
+            {
+                outGrid[y][x] = false;
+                continue;
+            }
+
+            if (CheckAround(y, x) < 4)
+            {
+                outGrid[y][x] = false;
+                stepCount++;
+                continue;
+            }
+            
+            outGrid[y][x] = true;
+        }
+    }
+    Console.WriteLine(stepCount);
+    
+    count += stepCount;
+    grid = outGrid;
+} while (stepCount > 0);
+
+Console.WriteLine(count);
+
+
+int CheckAround(int y, int x) => AllAround(y, x).Select(((int y, int x) coords) => grid[coords.y][coords.x] ? 1 : 0).Sum();
+
+IEnumerable<(int, int)> AllAround(int y, int x)
+{
+    if (y > 0)
+    {
+        if (x > 0)
+        {
+            yield return (y - 1, x - 1);
+        }
+
+        yield return (y - 1, x);
+
+        if (x < grid[0].Length - 1)
+        {
+            yield return (y - 1, x + 1);
+        }
+    }
+
+    if (x < grid[0].Length - 1)
+    {
+        yield return (y, x + 1);
+    }
+
+    if (x > 0)
+    {
+        yield return (y, x - 1);
+    }
+
+    if (y < grid.Count - 1)
+    {
+        if (x > 0)
+        {
+            yield return (y + 1, x - 1);
+        }
+
+        yield return (y + 1, x);
+        
+        if (x < grid[0].Length - 1)
+        {
+            yield return (y + 1, x + 1);
+        }
+    }
+}
