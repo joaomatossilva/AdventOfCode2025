d86da3f829ed67f403072c5127042f486351beee
João Filipe Matos Silva <joao@kspace.pt>
2025-12-05 09:27:58 +0000

    Day 5

---
+++ Puzzle10/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input.txt");
+
+var ranges = new List<Range>();
+
+foreach (var line in lines)
+{
+    var rangeMatch = Reg.GetRanges().Match(line);
+    if (rangeMatch.Success)
+    {
+        var start = long.Parse(rangeMatch.Groups[1].Value);
+        var end = long.Parse(rangeMatch.Groups[2].Value);
+        var range =  new Range(start, end);
+
+        var listToRemove = new List<Range>();
+        foreach (var range1 in ranges)
+        {
+            range = range.Adjust(range1,  out bool removeOther);
+            if (removeOther)
+            {
+                listToRemove.Add(range1);
+            }
+        }
+
+        ranges = ranges.Except(listToRemove).ToList();
+        
+        if(range.Length > 0)
+            ranges.Add(range);
+    }
+}
+
+long count = 0;
+foreach (var range in ranges)
+{
+    count += range.Length;
+}
+
+Console.WriteLine($"Count: {count}");
+
+partial class Reg
+{
+    [GeneratedRegex("^(\\d+)-(\\d+)$")]
+    public static partial Regex GetRanges();
+}
+
+record Range(long Start, long End)
+{
+    public long Length => End - Start + 1;
+
+    public Range Adjust(Range other, out bool removeOther)
+    {
+        removeOther = false;
+        long start = Start;
+        long end = End;
+        
+        if (Start <= other.End && Start >= other.Start)
+        {
+            start = other.End + 1;
+        }
+
+        if (End >= other.Start && End <= other.End)
+        {
+            end = other.Start - 1;
+        }
+        
+        if (End >= other.End && Start <= other.Start)
+        {
+            removeOther =  true;
+            return this;
+        }
+        
+        if (End <= other.End && Start >= other.Start)
+        {
+            start = 1;
+            end = 0;
+        }
+        
+        return new Range(Start: start, End: end);
+    }
+}
---
+++ Puzzle9/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input.txt");
+
+var ranges = new List<Range>();
+var ids = new List<long>();
+
+foreach (var line in lines)
+{
+    var rangeMatch = Reg.GetRanges().Match(line);
+    if (rangeMatch.Success)
+    {
+        var start = long.Parse(rangeMatch.Groups[1].Value);
+        var end = long.Parse(rangeMatch.Groups[2].Value);
+        ranges.Add(new Range(start, end));
+        continue;
+    }
+    
+    var idsMatch = Reg.GetIds().Match(line);
+    if (idsMatch.Success)
+    {
+        ids.Add(long.Parse(idsMatch.Groups[1].Value));
+    }
+}
+
+var count = 0;
+foreach (var id in ids)
+{
+    foreach (var range in ranges)
+    {
+        if (range.IsInside(id))
+        {
+            count++;
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"Count: {count}");
+
+partial class Reg
+{
+    [GeneratedRegex("^(\\d+)-(\\d+)$")]
+    public static partial Regex GetRanges();
+
+    [GeneratedRegex("^(\\d+)$")]
+    public static partial Regex GetIds();
+}
+
+record Range(long Start, long End)
+{
+    public bool IsInside(long x) => x >= Start && x <= End;
+}
