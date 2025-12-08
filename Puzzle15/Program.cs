df6174a8e0e691086bee5e0311e060fbfecb220c
João Filipe Matos Silva <joao@kspace.pt>
2025-12-08 11:17:38 +0000

    Day 8

---
+++ Puzzle15/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input.txt");
+
+var smallest = new PriorityQueue<Pair, double>();
+var nodes = new Dictionary<JuntionBox, List<JuntionBox>>();
+
+for (int i = 0; i < lines.Length; i++)
+{
+    var line = lines[i];
+    var match = Reg.GetCoords().Match(line);
+    if(!match.Success)
+        continue;
+    
+    nodes.Add(new JuntionBox(int.Parse(match.Groups[1].Value),  int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value)), new List<JuntionBox>());
+}
+
+var keys = nodes.Keys.ToArray();
+for (int i = 0; i < keys.Length; i++)
+{
+    for (int j = i + 1; j < keys.Length; j++)
+    {
+        var distance = keys[i].DistanceTo(keys[j]);
+        smallest.Enqueue(new Pair(keys[i], keys[j], distance), Int32.MaxValue - distance);
+        if(smallest.Count > 1000)
+        {
+            smallest.Dequeue();
+        }
+    }
+}
+
+var smallPairs = smallest.UnorderedItems.OrderBy(x => x.Element.Distance).Select(x => x.Element).Take(1000);
+
+foreach (var pair in smallPairs)
+{
+    nodes[pair.A].Add(pair.B);
+    nodes[pair.B].Add(pair.A);
+}
+
+HashSet<JuntionBox> seen = new();
+var circuits = new List<HashSet<JuntionBox>>();
+
+foreach (var node in nodes)
+{
+    if (!seen.Add(node.Key))
+    {
+        continue;
+    }
+    HashSet<JuntionBox> connections = new HashSet<JuntionBox>();
+    AddNode(node.Key, connections);
+    circuits.Add(connections);
+}
+
+void AddNode(JuntionBox box, HashSet<JuntionBox> connections)
+{
+    connections.Add(box);
+    foreach (var connected in nodes[box])
+    {
+        if (!seen.Add(connected))
+        {
+            continue;
+        }
+        AddNode(connected, connections);
+    }
+}
+
+var smallestConnected = circuits.Select(x => x.Count).OrderByDescending(x => x).Take(3);
+
+
+Console.WriteLine($"Smallest product: {smallestConnected.Aggregate((x, x1) => x * x1)}");
+
+record JuntionBox(int X, int Y, int Z)
+{
+    public double DistanceTo(JuntionBox box) => Math.Pow(box.X - X, 2) + Math.Pow(box.Y - Y, 2) + Math.Pow(box.Z - Z, 2);
+}
+
+record Pair(JuntionBox A, JuntionBox B, double Distance);
+
+partial class Reg
+{
+    [GeneratedRegex("^(\\d+),(\\d+),(\\d+)$")]
+    public static partial Regex GetCoords();
+}
---
+++ Puzzle16/Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input.txt");
+
+var pairs = new List<Pair>();
+var nodes = new Dictionary<JuntionBox, List<JuntionBox>>();
+
+for (int i = 0; i < lines.Length; i++)
+{
+    var line = lines[i];
+    var match = Reg.GetCoords().Match(line);
+    if(!match.Success)
+        continue;
+    
+    nodes.Add(new JuntionBox(int.Parse(match.Groups[1].Value),  int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value)), new List<JuntionBox>());
+}
+
+var keys = nodes.Keys.ToArray();
+for (int i = 0; i < keys.Length; i++)
+{
+    for (int j = i + 1; j < keys.Length; j++)
+    {
+        var distance = keys[i].DistanceTo(keys[j]);
+        pairs.Add(new Pair(keys[i], keys[j], distance));
+    }
+}
+
+var smallPairs = pairs.OrderBy(x => x.Distance);
+
+var circuits = new List<HashSet<JuntionBox>>();
+
+foreach (var smallPair in smallPairs)
+{
+    var circuitOfA = GetCircuitOf(smallPair.A);
+    var circuitOfB = GetCircuitOf(smallPair.B);
+
+    if (circuitOfA == null && circuitOfB == null)
+    {
+        circuits.Add([smallPair.A, smallPair.B]);
+    }
+    else if(circuitOfA != null && circuitOfB == null)
+    {
+        circuitOfA.Add(smallPair.B);
+    }
+    else if(circuitOfB != null && circuitOfA == null)
+    {
+        circuitOfB.Add(smallPair.A);
+    }
+    else if (circuitOfA == circuitOfB) // same circuit, don't do anything
+    {
+        continue;
+    }
+    else
+    {
+        //move all of B into A
+        foreach (var juntionBox in circuitOfB)
+        {
+            circuitOfA.Add(juntionBox);
+        }
+
+        circuits.Remove(circuitOfB);
+    }
+
+    if (circuits.Count == 1 && circuits[0].Count == nodes.Count)
+    {
+        Console.WriteLine($"Found Node at {smallPair.A.X} * {smallPair.B.X} = {smallPair.A.X *  smallPair.B.X}");
+        return;
+    }
+}
+
+Console.WriteLine("Not Found");
+
+HashSet<JuntionBox>? GetCircuitOf(JuntionBox box) => circuits.FirstOrDefault(circuit => circuit.Contains(box));
+
+record JuntionBox(int X, int Y, int Z)
+{
+    public double DistanceTo(JuntionBox box) => Math.Pow(box.X - X, 2) + Math.Pow(box.Y - Y, 2) + Math.Pow(box.Z - Z, 2);
+}
+
+record Pair(JuntionBox A, JuntionBox B, double Distance);
+
+partial class Reg
+{
+    [GeneratedRegex("^(\\d+),(\\d+),(\\d+)$")]
+    public static partial Regex GetCoords();
+}
